eb290a963da1e0e26db4400d4a235979e19a8e4a
edalzell <erin@thedalzells.org>
2020-01-30 14:50:28 -0800

    register cashier

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Silentz\Charge;
 
+use Laravel\Cashier\CashierServiceProvider;
 use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
@@ -9,4 +10,9 @@ class ServiceProvider extends AddonServiceProvider
     protected $routes = [
         'actions' => __DIR__ . '/../routes/actions.php',
     ];
+
+    public function register()
+    {
+        $this->app->register(CashierServiceProvider::class);
+    }
 }
