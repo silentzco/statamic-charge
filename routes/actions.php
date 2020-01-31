1d3c2d4df13b74c3eee519df429d8cb08a12fc11
edalzell <erin@thedalzells.org>
2020-01-30 21:30:39 -0800

    rename to be “standard”
    not sure I like the names though…

--- routes/actions.php
+++ routes/actions.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Silentz\Charge\Http\Controllers\OneTimeController;
 use Silentz\Charge\Http\Controllers\SubscriptionController;
 
-Route::post('one-time', [OneTimeController::class, 'store'])
-    ->name('charge.one-time.store');
+Route::post('one-time', [OneTimeController::class, 'store'])->name('charge.one-time.store');
 
 // Subscriptions
 Route::get('subscription/{subscription}', [SubscriptionController::class, 'show'])->name('charge.subscription.show');
-Route::post('subscription', [SubscriptionController::class, 'store'])->name('charge.subscription.create');
+Route::post('subscription', [SubscriptionController::class, 'store'])->name('charge.subscription.store');
 Route::delete('subscription/{subscription}', [SubscriptionController::class, 'destroy'])->name('charge.subscription.destroy');
