244456b89365688ff573be12b278b5f02bf8f61a
edalzell <erin@thedalzells.org>
2020-02-01 14:26:49 -0800

    switch to subscription name

--- routes/actions.php
+++ routes/actions.php
@@ -7,6 +7,6 @@ use Silentz\Charge\Http\Controllers\SubscriptionController;
 Route::post('one-time', [OneTimeController::class, 'store'])->name('charge.one-time.store');
 
 // Subscriptions
-Route::get('subscription/{subscription}', [SubscriptionController::class, 'show'])->name('charge.subscription.show');
+Route::get('subscription/{name}', [SubscriptionController::class, 'show'])->name('charge.subscription.show');
 Route::post('subscription', [SubscriptionController::class, 'store'])->name('charge.subscription.store');
-Route::delete('subscription/{subscription}', [SubscriptionController::class, 'destroy'])->name('charge.subscription.destroy');
+Route::delete('subscription/{name}', [SubscriptionController::class, 'destroy'])->name('charge.subscription.destroy');
