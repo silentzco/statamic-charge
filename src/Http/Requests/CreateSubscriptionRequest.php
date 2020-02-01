b4cec7816cf0068d5e95ea8afd25c88f4579c0fd
edalzell <erin@thedalzells.org>
2020-02-01 14:27:05 -0800

    remove unnecessary comments

--- src/Http/Requests/CreateSubscriptionRequest.php
+++ src/Http/Requests/CreateSubscriptionRequest.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateSubscriptionRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
