dbf6af8957bfd1d7adeeda763d8183aa81117e7f
edalzell <erin@thedalzells.org>
2020-02-02 13:50:57 -0800

    organize the routes better

--- routes/actions.php
+++ routes/actions.php
@@ -4,9 +4,14 @@ use Illuminate\Support\Facades\Route;
 use Silentz\Charge\Http\Controllers\OneTimeController;
 use Silentz\Charge\Http\Controllers\SubscriptionController;
 
-Route::post('one-time', [OneTimeController::class, 'store'])->name('charge.one-time.store');
+Route::name('charge.')->group(function () {
+    Route::name('one-time.')->group(function () {
+        Route::post('one-time', [OneTimeController::class, 'store'])->name('one-time.store');
+    });
 
-// Subscriptions
-Route::get('subscription/{name}', [SubscriptionController::class, 'show'])->name('charge.subscription.show');
-Route::post('subscription', [SubscriptionController::class, 'store'])->name('charge.subscription.store');
-Route::delete('subscription/{name}', [SubscriptionController::class, 'destroy'])->name('charge.subscription.destroy');
+    Route::name('subscription.')->group(function () {
+        Route::get('subscription/{name}', [SubscriptionController::class, 'show'])->name('show');
+        Route::post('subscription', [SubscriptionController::class, 'store'])->name('store');
+        Route::delete('subscription/{name}', [SubscriptionController::class, 'destroy'])->name('destroy');
+    });
+});
