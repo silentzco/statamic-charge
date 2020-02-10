9e368593f50a6f510614bcbb36251a003cef9b59
edalzell <erin@thedalzells.org>
2020-02-09 21:25:37 -0800

    add the config

---
+++ config/charge.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'email' => [
+        'from' => '',
+        'one-time' => [
+            'payment_template' => '',
+            'payment_subject' => '',
+        ],
+        'subscription' => [
+            'updated_template' => '',
+            'updated_subject' => '',
+            'canceled_template' => '',
+            'canceled_subject' => '',
+            'upcoming_payment_template' => '',
+            'upcoming_payment_subject' => '',
+            'payment_succeeded_template' => '',
+            'payment_succeeded_subject' => '',
+            'payment_failed_template' => '',
+            'payment_failed_subject' => '',
+        ],
+    ],
+];
