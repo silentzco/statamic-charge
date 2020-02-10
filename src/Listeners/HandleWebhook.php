33e8ddfd9a62ab210c1056bde7aec1eed632e9cc
edalzell <erin@thedalzells.org>
2020-02-09 21:27:59 -0800

    send emails for all the Cashier events

--- src/Listeners/HandleWebhook.php
+++ src/Listeners/HandleWebhook.php
@@ -3,17 +3,29 @@
 namespace Silentz\Charge\Listeners;
 
 use Illuminate\Support\Facades\Mail;
+use Silentz\Charge\Mail\CustomerDeleted;
+use Silentz\Charge\Mail\CustomerUpdated;
 use Laravel\Cashier\Events\WebhookHandled;
+use Silentz\Charge\Mail\CustomerSubscriptionDeleted;
 use Silentz\Charge\Mail\CustomerSubscriptionUpdated;
+use Silentz\Charge\Mail\InvoicePaymentActionRequired;
 
 class HandleWebhook
 {
     public function handle(WebhookHandled $event)
     {
-        // send email in here, no need for other Events
+        $events = [
+            'customer.subscription.updated' => CustomerSubscriptionUpdated::class,
+            'customer.subscription.deleted' => CustomerSubscriptionDeleted::class,
+            'customer.updated' => CustomerUpdated::class,
+            'customer.deleted' => CustomerDeleted::class,
+            'invoice.payment_action_required' => InvoicePaymentActionRequired::class,
+        ];
 
-        // get the right mailable, pass the payload in
+        $class = $events[$event->payload['type']];
 
-        Mail::to('foo@bar.com')->send(new CustomerSubscriptionUpdated($event->payload));
+        // should I grab the user and the subscription here to pass to the mailables?
+
+        Mail::to(config('charge.email.from'))->send(new $class($event->payload));
     }
 }
