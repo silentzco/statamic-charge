7f989fff8e28b08582e1b5ade949cb00bed92150
edalzell <erin@thedalzells.org>
2020-02-23 21:25:55 -0800

    adding roles (wip)

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -2,13 +2,33 @@
 
 namespace Silentz\Charge\Http\Controllers;
 
-use Laravel\Cashier\Http\Middleware\VerifyWebhookSignature;
+use Statamic\Facades\Role;
+use Statamic\Facades\User;
+use Laravel\Cashier\Cashier;
+use Illuminate\Http\Response;
+use Statamic\Auth\User as AuthUser;
+use Silentz\Charge\Models\User as UserModel;
 use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
 
 class WebhookController extends CashierController
 {
-    public function __construct()
-    {
-        $this->middleware(VerifyWebhookSignature::class);
+    protected function handleCustomerSubscriptionCreated(
+        array $payload
+    ): Response {
+        // set roles
+        /** @var AuthUser */
+        //        $user = User::fromUser(Cashier::findBillable($payload['data']['object']['id']));
+
+        $user = User::fromUser(
+            UserModel::where('email', 'add-roles@cashier-test.com')->first()
+        );
+
+        $user->assignRole('foo');
+
+        $user->save();
+
+        dd(Role::all());
+
+        return new Response();
     }
 }
