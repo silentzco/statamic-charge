0b3e87ec3b4ba5a354c4793c1c420747c07b6036
edalzell <erin@thedalzells.org>
2020-03-28 18:00:27 -0700

    bring in Vue components

---
+++ resources/js/cp.js
@@ -0,0 +1,12 @@
+// import CommerceCreateForm from "./components/Publish/CommerceCreateForm";
+
+
+Statamic.booting(() => {
+    // Statamic.$components.register('commerce-create-form', CommerceCreateForm);
+    // Statamic.$components.register('customer-orders-fieldtype', CustomerOrdersFieldtype);
+    // Statamic.$components.register('money-fieldtype', MoneyFieldtype);
+    // Statamic.$components.register('order-items-fieldtype', OrderItemsFieldtype);
+    // Statamic.$components.register('order-status-settings', OrderStatus);
+    // Statamic.$components.register('shipping-zone-settings', ShippingZone);
+    // Statamic.$components.register('tax-rate-settings', TaxRate);
+});
---
+++ webpack.mix.js
@@ -0,0 +1,16 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+// WIP: work on a better solution that just copying over...
+
+mix.js('resources/js/cp.js', 'dist/js/cp.js');
