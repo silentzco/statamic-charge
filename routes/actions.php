36c1b8324e18931606135f8ff6a40b7fd327965b
edalzell <erin@thedalzells.org>
2020-04-12 18:09:38 -0700

    make routes explicit

--- routes/actions.php
+++ routes/actions.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use Silentz\Charge\Http\Controllers\WebhookController;
+use Silentz\Charge\Http\Controllers\Web\SubscriptionController;
 
 Route::name('charge.')->group(function () {
-    Route::apiResource('subscription', 'Http\Controllers\Web\SubscriptionController');
-
     Route::post('webhook', [WebhookController::class, 'handleWebhook'])->name('webhook');
+    Route::middleware('auth')->group(function () {
+        Route::name('subscription.')->group(function () {
+            Route::get('subscription/{subscription}', [SubscriptionController::class, 'show'])->name('get');
+            Route::post('subscription', [SubscriptionController::class, 'store'])->name('store');
+            Route::patch('subscription/{subscription}', [SubscriptionController::class, 'update'])->name('update');
+            Route::delete('subscription/{subscription}', [SubscriptionController::class, 'destroy'])->name('destroy');
+        });
+    });
 });
--- routes/cp.php
+++ routes/cp.php
@@ -1,7 +1,21 @@
 <?php
 
+use Silentz\Charge\Models\User;
+use Laravel\Cashier\Subscription;
 use Illuminate\Support\Facades\Route;
+use Silentz\Charge\Http\Controllers\Cp\SubscriptionController;
 
 Route::name('charge.')->prefix('charge')->group(function () {
-    Route::apiResource('subscription', 'Http\Controllers\Cp\SubscriptionController');
+    Route::redirect('/', 'charge/subscriptions')->name('index');
+    Route::name('subscription.')->group(function () {
+        Route::view('subscriptions', 'charge::cp.subscriptions', ['subscriptions' => Subscription::with('user')->get()])
+            ->name('index');
+        Route::delete('subscription/{subscription}', [SubscriptionController::class, 'destroy'])
+            ->name('destroy');
+    });
+
+    Route::name('customer.')->group(function () {
+        Route::view('customers', 'charge::cp.customers', ['customers' => User::customers()->get()])
+            ->name('index');
+    });
 });
