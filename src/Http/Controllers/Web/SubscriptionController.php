b99340bcf6b8451fda0aa114a7bce75bf3fa66e5
edalzell <erin@thedalzells.org>
2020-04-22 22:22:40 -0700

    wip

--- src/Http/Controllers/Web/SubscriptionController.php
+++ src/Http/Controllers/Web/SubscriptionController.php
@@ -2,50 +2,83 @@
 
 namespace Silentz\Charge\Http\Controllers\Web;
 
+use Stripe\ErrorObject;
 use Illuminate\Http\Request;
 use Laravel\Cashier\Subscription;
 use Illuminate\Http\RedirectResponse;
+use Stripe\Exception\ApiErrorException;
 use Statamic\Http\Controllers\Controller;
-use Silentz\Charge\Http\Middleware\HasSubscription;
+use Silentz\Charge\Http\Requests\CancelSubscriptionRequest;
 use Silentz\Charge\Http\Requests\CreateSubscriptionRequest;
 use Silentz\Charge\Http\Requests\UpdateSubscriptionRequest;
 
 class SubscriptionController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(HasSubscription::class)->except('store');
-    }
-
-    public function show(Subscription $subscription): ?Subscription
-    {
-        return $subscription;
-    }
-
-    public function store(CreateSubscriptionRequest $request): Subscription
+    public function store(CreateSubscriptionRequest $request): RedirectResponse
     {
         $request->validated();
 
-        return current_user()
-            ->newSubscription($request->name, $request->plan)
-            ->create($request->payment_method);
+        try {
+            $subscription = current_user()
+              ->newSubscription($request->name, $request->plan)
+              ->create($request->payment_method);
+
+            return $this->withSuccess($subscription, $request);
+        } catch (ApiErrorException $e) {
+            return $this->withErrors($e->getError(), 'create');
+        }
     }
 
-    public function update(Subscription $subscription, UpdateSubscriptionRequest $request): Subscription
+    public function update(Subscription $subscription, UpdateSubscriptionRequest $request): RedirectResponse
     {
+        $request->validated();
+
         $plan = $request->get('plan');
 
-        if ($plan != $subscription->stripe_plan) {
-            $subscription->swap($plan);
-        }
+        try {
+            if ($plan != $subscription->stripe_plan) {
+                $subscription->swap($plan);
+            }
 
-        return $subscription->updateQuantity($request->get('quantity', 1));
+            return $this->withSuccess(
+                    $subscription->updateQuantity($request->get('quantity', 1)),
+                    $request
+            );
+        } catch (ApiErrorException $e) {
+            return $this->withErrors($e->getError(), 'update');
+        }
     }
 
-    public function destroy(Subscription $subscription, Request $request): RedirectResponse
+    public function destroy(Subscription $subscription, CancelSubscriptionRequest $request): RedirectResponse
     {
-        $subscription = $request->cancel_immediately ? $subscription->cancelNow() : $subscription->cancel();
+        try {
+            $subscription = $request->cancel_immediately ? $subscription->cancelNow() : $subscription->cancel();
 
-        return $request->redirect ? redirect($request->redirect) : back();
+            return $this->withSuccess($subscription, $request);
+        } catch (ApiErrorException $e) {
+            return $this->withErrors($e->getError(), 'cancel');
+        }
+    }
+
+    private function withSuccess(Subscription $subscription, Request $request): RedirectResponse
+    {
+        $redirect = $request->input('redirect', false);
+
+        $response = $redirect ? redirect($redirect) : back();
+
+        return $response->with([
+          'success' => true,
+          'subscription' = $subscription->toArray()
+        ]);
+    }
+
+    private function withErrors(ErrorObject $error, string $bag): RedirectResponse
+    {
+        return back()->withErrors([
+            'type' => $error->type,
+            'code' => $error->code,
+            'param' => $error->param,
+            'message' => $error->message
+        ], $bag);
     }
 }
