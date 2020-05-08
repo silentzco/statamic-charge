6d2bfc49a8323a23a0959c206164bd46838ad3f8
edalzell <erin@thedalzells.org>
2020-05-07 21:43:08 -0700

    Handle incomplete payments and proper scoping of statuses

--- src/Http/Controllers/Web/SubscriptionController.php
+++ src/Http/Controllers/Web/SubscriptionController.php
@@ -4,6 +4,8 @@ namespace Silentz\Charge\Http\Controllers\Web;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Laravel\Cashier\Cashier;
+use Laravel\Cashier\Exceptions\IncompletePayment;
 use Laravel\Cashier\Subscription;
 use Silentz\Charge\Http\Requests\CreateSubscriptionRequest;
 use Silentz\Charge\Http\Requests\SubscriptionRequest;
@@ -11,6 +13,7 @@ use Silentz\Charge\Http\Requests\UpdateSubscriptionRequest;
 use Statamic\Http\Controllers\Controller;
 use Stripe\ErrorObject;
 use Stripe\Exception\ApiErrorException;
+use Stripe\PaymentIntent;
 
 class SubscriptionController extends Controller
 {
@@ -24,6 +27,8 @@ class SubscriptionController extends Controller
             return $this->withSuccess($subscription, $request);
         } catch (ApiErrorException $e) {
             return $this->withErrors($e->getError(), $request);
+        } catch (IncompletePayment $e) {
+            return $this->withIncomplete($e->payment->id, $request);
         }
     }
 
@@ -62,22 +67,34 @@ class SubscriptionController extends Controller
 
     private function withSuccess(Subscription $subscription, Request $request): RedirectResponse
     {
-        $redirect = $request->input('redirect', false);
-
-        $response = $redirect ? redirect($redirect) : back();
-
-        return $response->with([
-          'success' => true,
-          'subscription' => $subscription->toArray(),
+        return $this->redirect('redirect', $request)->with([
+            'charge' => [
+                'success' => true,
+                'subscription' => $subscription->toArray(),
+            ],
         ]);
     }
 
     private function withErrors(ErrorObject $error, Request $request): RedirectResponse
     {
-        $redirect = $request->input('error_redirect', false);
+        return $this->redirect('error_redirect', $request)->withErrors($error->message, 'charge');
+    }
 
-        $response = $redirect ? redirect($redirect) : back();
+    private function withIncomplete($id, Request $request): RedirectResponse
+    {
+        return $this->redirect('action_needed_redirect', $request)->with([
+            'charge' => [
+                'requires_action' => true,
+                'action' => 'confirm',
+                'payment_intent' => PaymentIntent::retrieve($id, Cashier::stripeOptions())->toArray(),
+            ],
+        ]);
+    }
 
-        return $response->withErrors($error->message, 'charge');
+    private function redirect($field, Request $request): RedirectResponse
+    {
+        $redirect = $request->input($field, false);
+
+        return $redirect ? redirect($redirect) : back();
     }
 }
