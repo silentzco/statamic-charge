1c2192fad207b4032f05c67b26cdc9dee07ce188
edalzell <erin@thedalzells.org>
2020-05-25 15:08:49 -0700

    not using vue component anymore

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,8 +1,6 @@
 import SubscriptionActions from "./components/SubscriptionActions";
-import RolePlans from "./components/RolePlans";
 
 
 Statamic.booting(() => {
     Statamic.$components.register('subscription-actions', SubscriptionActions);
-    Statamic.$components.register('role-plans', RolePlans);
 });
