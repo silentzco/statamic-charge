ed78fbce026be278daa058b89203dbaca5cb9c50
edalzell <erin@thedalzells.org>
2020-07-19 10:58:10 -0700

    Chargeable trait

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,26 +3,9 @@
 namespace Silentz\Charge\Models;
 
 use Illuminate\Foundation\Auth\User as Model;
-use Laravel\Cashier\Billable;
-use Statamic\Facades\User as UserAPI;
-use Statamic\Support\Arr;
+use Silentz\Charge\Traits\Charge;
 
 class User extends Model
 {
-    use Billable;
-
-    public function swapPlans($newPlan, $oldPlan = null)
-    {
-        $roles = collect(config('charge.subscription.roles'));
-
-        UserAPI::fromUser($this)
-            ->removeRole($this->getRole($roles, $oldPlan))
-            ->assignRole($this->getRole($roles, $newPlan))
-            ->save();
-    }
-
-    private function getRole($roles, $plan)
-    {
-        return Arr::get($roles->firstWhere('plan', $plan), 'role');
-    }
+    use Charge;
 }
---
+++ src/Traits/Chargeable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Silentz\Charge\Traits;
+
+use Laravel\Cashier\Billable;
+use Statamic\Facades\User;
+use Statamic\Support\Arr;
+
+trait Chargeable
+{
+    use Billable;
+
+    public function swapPlans($newPlan, $oldPlan = null)
+    {
+        $roles = collect(config('charge.subscription.roles'));
+
+        User::fromUser($this)
+            ->removeRole($this->getRole($roles, $oldPlan))
+            ->assignRole($this->getRole($roles, $newPlan))
+            ->save();
+    }
+
+    private function getRole($roles, $plan)
+    {
+        return Arr::get($roles->firstWhere('plan', $plan), 'role');
+    }
+}
