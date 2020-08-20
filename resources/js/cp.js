121d7c76d95bb298748d76130cf4d90d0d2a86a5
edalzell <erin@thedalzells.org>
2020-08-19 21:27:55 -0700

    spacing

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,6 +1,5 @@
 import SubscriptionActions from "./components/SubscriptionActions";
 
-
 Statamic.booting(() => {
     Statamic.$components.register('subscription-actions', SubscriptionActions);
 });
