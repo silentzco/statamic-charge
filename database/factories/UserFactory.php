217f17588734874338041253f5b87502c1ec3e77
edalzell <erin@silentz.co>
2022-09-08 20:42:34 -0700

    upgrade factories to current standards

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,28 +1,32 @@
 <?php
 
-use Faker\Generator as Faker;
+namespace Silentz\Charge\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Silentz\Charge\Models\User;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
-        'stripe_id' => $faker->md5,
-        'card_brand' => $faker->creditCardType,
-        'card_last_four' => substr($faker->creditCardNumber, -4),
-        'trial_ends_at' => $faker->creditCardExpirationDate,
-    ];
-});
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+        ];
+    }
+}
